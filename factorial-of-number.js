be9e3b92df768851d2558af4944bb46eee69fcc5
b0yblake <b0yblakecrzy@gmail.com>
2020-12-13 23:29:12 +0700

    up

--- factorial-of-number.js
+++ factorial-of-number.js
@@ -11,12 +11,12 @@ const factorialize1 = (num) => {
     if(!num || num < 0) return false
     if(num === 0) return 1
     let i = 1,
-    tmp = 1
+        tmp = 1;
     while (i <= num) {
         tmp *= i
         i++
     }
-    return tmp
+    return tmp;
 }
 console.log(factorialize1(6));
 
@@ -24,7 +24,6 @@ console.log(factorialize1(6));
 const factorialize2 = (num) => {
     if(!num || num < 0) return false
     if(num === 0) return 1
-
     return num * factorialize2(num-1)
 }
-console.log(factorialize1(6));
+console.log(factorialize2(6));
