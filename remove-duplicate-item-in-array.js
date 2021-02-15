b6f1490287a196371c925dc5bf26a5e8d3fe7484
b0yblake <lichnh.fe@gmail.com>
2021-02-15 23:01:50 +0700

    daily update

--- remove-duplicate-item-in-array.js
+++ remove-duplicate-item-in-array.js
@@ -7,7 +7,7 @@
 
 const arrayDuplicated = [1, 2, 2, 3, 1, 2, 4, 5, 4, 2, 6];
 
-// Cách 1: new Set
+// Cách 1: new Set (best way for model js)
 const removedDuplicate = array => [...new Set(array)];
 console.log(removedDuplicate(arrayDuplicated)); //[1, 2, 3, 4, 5, 6]
 
@@ -22,20 +22,18 @@ const removedDuplicate2 = (array) => {
 console.log(removedDuplicate2(arrayDuplicated));
 
 // Cách 3: Sử dụng filter
-// const removedDuplicate3 = (array) => {
-//   let tmpArray = [];
-//   array.filter(item => {
-//     if(!tmpArray.includes(item)) tmpArray.push(item);
-//   })
-//   return tmpArray;
-// }
-// console.log(removedDuplicate3(arrayDuplicated));
+const removedDuplicate3 = (array) => {
+  return array.filter((item, index, self) => {
+    return self.indexOf(item) === index;
+  })
+}
+console.log(removedDuplicate3(arrayDuplicated));
 
-// // Cách 4: Sử dụng reduce -> init []
-// const removedDuplicate4 = array => {
-//   array.reduce((acc, cur) => {
-//     if (acc.includes(cur) < 0 ) acc.push(cur);
-//     return acc;
-//   },[]);
-// }
-// console.log(removedDuplicate4(arrayDuplicated));
+// Cách 4: Sử dụng reduce -> init []
+const removedDuplicate4 = array => {
+  return array.reduce((acc, cur) => {
+    if (!acc.includes(cur)) acc.push(cur);
+    return acc;
+  },[]);
+}
+console.log(removedDuplicate4(arrayDuplicated));
